762a9bf690813c706caadeb1e7b1bc74906c9fbb
dioen <piotr.b.ek@gmail.com>
2017-09-12 21:04:44 +0200

    Adding database

--- WebApplication1/WebApplication1/Controllers/LoginController.cs
+++ WebApplication1/WebApplication1/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -11,6 +12,27 @@ namespace WebApplication1.Controllers
         // GET: Login
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserModel User)
+        {
+            using (UserDbContext UserDB = new UserDbContext())
+            {
+                if (ModelState.IsValid)
+                {
+                    var cos = User;
+                    var _User = UserDB.Users.Where(record => record.Mail.Equals(User.Mail) && record.Password.Equals(User.Password));
+                    var asd = _User;
+                    var cc = asd;
+                    if (_User != null)
+                    {
+                        return View();
+                    }
+                }
+            }
+
             return View();
         }
     }
