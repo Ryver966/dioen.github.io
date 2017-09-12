51aca3f872c51e5eb9b01bee1cbd833cf18c73ea
dioen <piotr.b.ek@gmail.com>
2017-09-12 16:33:56 +0200

    Uploading project for ASP.NET

---
+++ WebApplication1/WebApplication1/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class UserModel : DbContext
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public double TotalWallet { get; set; }
+    }
+
+    public class UserDbContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
